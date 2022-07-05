8d8544917627e87e3d6a7e5beec4b15bfb16dfdd
Alexander Bukhalo <alex@bukhalo.com>
2022-07-06 01:41:44 +0300

    aside menu

--- src/ui/aside/aside.tsx
+++ src/ui/aside/aside.tsx
@@ -1,5 +1,4 @@
 import React, { FormEvent, useState } from 'react';
-import { NavLink } from 'react-router-dom';
 import { DayPicker } from '../day-picker/day-picker';
 import s from './aside.module.css';
 import { AsideResizable } from './aside-resizable';
@@ -7,6 +6,9 @@ import {
   useProjectAddMutation,
   useProjectCollectionQuery,
 } from '../../generated/graphql';
+import { MenuGroup } from '../menu/menu-group';
+import { Section } from '../menu/section';
+import { LinkItem } from '../menu/link-item';
 
 export interface AsideProps {
   readonly isVisible: boolean;
@@ -31,62 +33,53 @@ export const Aside: React.FC<AsideProps> = ({ isVisible }) => {
       <AsideResizable minWidth={250} maxWidth={600}>
         <div style={{ margin: '1rem' }}>
           <DayPicker />
-          <div className={s.menu}>
-            <span className={s['menu-title']}>Filters</span>
-            <nav>
-              <ul>
-                <li>
-                  <NavLink
-                    to={`/filter/trash`}
-                    className={({ isActive }) =>
-                      isActive
-                        ? `${s['menu-title']} ${s['menu-title--active']}`
-                        : s['menu-title']
-                    }
-                  >
-                    Trash
-                  </NavLink>
-                </li>
-              </ul>
-            </nav>
-          </div>
-          <div className={s.menu}>
-            <span className={s['menu-title']}>Projects</span>
-            <nav>
-              {loading ? null : (
-                <ul>
-                  {data!.projectCollection!.map((project: any) => (
-                    <li key={project.id}>
-                      <NavLink
-                        to={`/project/${project.id}`}
-                        className={({ isActive }) =>
-                          isActive
-                            ? `${s['menu-title']} ${s['menu-title--active']}`
-                            : s['menu-title']
-                        }
-                      >
-                        {project.name}
-                      </NavLink>
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </nav>
-            <form onSubmit={onProjectAddSubmit}>
-              <input
-                type="text"
-                placeholder={'Project name'}
-                value={projectAddForm.name}
-                onChange={event =>
-                  setProjectAddForm({
-                    ...projectAddForm,
-                    name: event.target.value,
-                  })
+          <MenuGroup>
+            <Section title={'Filters'}>
+              <LinkItem
+                to={`/filter/trash`}
+                className={({ isActive }) =>
+                  isActive
+                    ? `${s['menu-title']} ${s['menu-title--active']}`
+                    : s['menu-title']
                 }
-              />
-              <button type={'submit'}>Add</button>
-            </form>
-          </div>
+              >
+                Trash
+              </LinkItem>
+            </Section>
+            <Section title={'Projects'}>
+              {loading ? null : (
+                <>
+                  {data!.projectCollection!.map((project: any) => (
+                    <LinkItem
+                      key={project.id}
+                      to={`/project/${project.id}`}
+                      className={({ isActive }) =>
+                        isActive
+                          ? `${s['menu-title']} ${s['menu-title--active']}`
+                          : s['menu-title']
+                      }
+                    >
+                      {project.name}
+                    </LinkItem>
+                  ))}
+                </>
+              )}
+            </Section>
+          </MenuGroup>
+          <form onSubmit={onProjectAddSubmit}>
+            <input
+              type="text"
+              placeholder={'Project name'}
+              value={projectAddForm.name}
+              onChange={event =>
+                setProjectAddForm({
+                  ...projectAddForm,
+                  name: event.target.value,
+                })
+              }
+            />
+            <button type={'submit'}>Add</button>
+          </form>
         </div>
       </AsideResizable>
     </div>
